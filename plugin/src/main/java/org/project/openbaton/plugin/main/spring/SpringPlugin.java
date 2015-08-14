5dd2099c644267cab3e304822570ce5fdc430b42
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-08-14 10:54:20 +0200

    improved plugin-sdk

--- plugin/src/main/java/org/project/openbaton/plugin/main/spring/SpringPlugin.java
+++ plugin/src/main/java/org/project/openbaton/plugin/main/spring/SpringPlugin.java
@@ -8,7 +8,8 @@ import org.project.openbaton.plugin.interfaces.main.Plugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,20 +20,21 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerEndpointRegistrar;
 import org.springframework.jms.config.SimpleJmsListenerEndpoint;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 
 import javax.annotation.PostConstruct;
-import javax.jms.*;
-import java.io.Serializable;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.ObjectMessage;
 import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by lto on 13/08/15.
  */
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan(basePackages = "org.project.openbaton")
 @EnableJms
-public class SpringPlugin extends Plugin implements MessageListener, JmsListenerConfigurer {
+public class SpringPlugin extends Plugin implements JmsListenerConfigurer {
 
     @Autowired
     private JmsTemplate jmsTemplate;
@@ -69,17 +71,6 @@ public class SpringPlugin extends Plugin implements MessageListener, JmsListener
         registrar.registerEndpoint(endpoint);
     }
 
-    private MessageCreator getObjectMessageCreator(final Serializable message) {
-        MessageCreator messageCreator = new MessageCreator() {
-            @Override
-            public Message createMessage(Session session) throws JMSException {
-                ObjectMessage objectMessage = session.createObjectMessage(message);
-                return objectMessage;
-            }
-        };
-        return messageCreator;
-    }
-
     @Override
     public void onMessage(Message message) {
         PluginMessage msg = null;
@@ -100,7 +91,7 @@ public class SpringPlugin extends Plugin implements MessageListener, JmsListener
             e.printStackTrace();
         }
 
-        this.jmsTemplate.send(pluginEndpoint, getObjectMessageCreator(answer));
+        pluginSender.send(pluginEndpoint, answer);
     }
 
     @PostConstruct
@@ -111,7 +102,10 @@ public class SpringPlugin extends Plugin implements MessageListener, JmsListener
     @Override
     protected void register() {
         log.debug("Registering plugin: " + endpoint);
-        this.jmsTemplate.send("plugin-register", getObjectMessageCreator(endpoint));
+        this.pluginSender.send("plugin-register", endpoint);
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlugin.class);
+    }
 }
--- plugin/src/main/java/org/project/openbaton/plugin/utils/AgentBroker.java
+++ plugin/src/main/java/org/project/openbaton/plugin/utils/AgentBroker.java
@@ -1,18 +1,23 @@
 package org.project.openbaton.plugin.utils;
 
 import org.project.openbaton.catalogue.nfvo.EndpointType;
-import org.project.openbaton.plugin.interfaces.agents.PluginReceiver;
 import org.project.openbaton.plugin.interfaces.agents.PluginSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by lto on 13/08/15.
  */
+@Service
+@Scope
 public class AgentBroker {
-    public static PluginSender getSender(EndpointType senderType) {
-        return null;
-    }
 
-    public static PluginReceiver getReceiver(EndpointType receiverType) {
-        return null;
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    public PluginSender getSender(EndpointType senderType) {
+        return (PluginSender) context.getBean(senderType.toString().toLowerCase() + "PluginSender");
     }
 }
