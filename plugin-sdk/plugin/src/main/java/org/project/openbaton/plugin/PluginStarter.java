dc7d3cb9fec33ade55da601d5e477e22c4a4eb20
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-09-10 16:22:35 +0200

    added hibernate

--- plugin-sdk/plugin/src/main/java/org/project/openbaton/plugin/PluginStarter.java
+++ plugin-sdk/plugin/src/main/java/org/project/openbaton/plugin/PluginStarter.java
@@ -48,4 +48,37 @@ protected static Logger log = LoggerFactory.getLogger(PluginStarter.class);
             }
         });
     }
+
+    public static void run(Class clazz, final String name, final String registryIp, int port){
+        try {
+            log.info("Starting plugin with name: " + name);
+            log.debug("Registry ip: " + registryIp);
+            log.debug("Class to register: " + clazz.getName());
+            StartupPlugin.register(clazz, name, registryIp, port);
+
+//            StartupPlugin.unregister(name, registryIp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
+
+        final Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                try {
+                    mainThread.join();
+                    log.info("Unregistering: " + name);
+                    StartupPlugin.unregister(name, registryIp);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (NotBoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
--- src/main/java/org/project/openbaton/clients/interfaces/client/openstack/Starter.java
+++ src/main/java/org/project/openbaton/clients/interfaces/client/openstack/Starter.java
@@ -1,21 +1,22 @@
 package org.project.openbaton.clients.interfaces.client.openstack;
 
 import org.project.openbaton.plugin.PluginStarter;
-import java.io.IOException;
-import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by lto on 10/09/15.
  */
 public class Starter {
-	    public static void main(String[] args) {
-		            Properties properties = new Properties();
-			            try {
-					                properties.load(Starter.class.getResourceAsStream("/plugin.conf.properties"));
-							        } catch (IOException e) {
-									            e.printStackTrace();
-										                System.exit(1);
-												        }
-				            PluginStarter.run(OpenstackClient.class, "openstack-plugin", properties.getProperty("registry-ip", "localhost"));
-					        }
+
+    private static Logger log = LoggerFactory.getLogger(PluginStarter.class);
+
+    public static void main(String[] args) {
+        log.info("params are: pluginName registryIp registryPort\ndefault is openstack-plugin localhost 1099");
+
+        if (args.length > 1)
+            PluginStarter.run(OpenstackClient.class, args[0] ,args[1], Integer.parseInt(args[2]));
+        else
+            PluginStarter.run(OpenstackClient.class, "openstack-plugin", "localhost");
+    }
 }
