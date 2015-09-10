a83cc54107012507e35fea5009084e7e8c947705
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-09-10 12:15:52 +0200

    improved starter class

--- src/main/java/org/project/openbaton/clients/interfaces/client/openstack/Starter.java
+++ src/main/java/org/project/openbaton/clients/interfaces/client/openstack/Starter.java
@@ -1,14 +1,21 @@
 package org.project.openbaton.clients.interfaces.client.openstack;
 
 import org.project.openbaton.plugin.PluginStarter;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by lto on 10/09/15.
  */
 public class Starter {
-    public static void main(String[] args) {
-        PluginStarter.run(OpenstackClient.class, "openstack-plugin", "localhost");
-
-
-    }
+	    public static void main(String[] args) {
+		            Properties properties = new Properties();
+			            try {
+					                properties.load(Starter.class.getResourceAsStream("/plugin.conf.properties"));
+							        } catch (IOException e) {
+									            e.printStackTrace();
+										                System.exit(1);
+												        }
+				            PluginStarter.run(OpenstackClient.class, "openstack-plugin", properties.getProperty("registry-ip", "localhost"));
+					        }
 }
